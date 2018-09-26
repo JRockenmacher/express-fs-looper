7413497d56f6d020a2d51e957b06b7992155fdf8
Vision-X <michael.s.marlow@gmail.com>
2018-09-26 17:42:44 -0600

    properly sifts data to a new JSON formatted object

--- index.js
+++ index.js
@@ -14,65 +14,84 @@ app.use(morgan('dev'));
 
 app.use(cors());
 
-var content = {};
-let name = '';
+
+
+
 let coordinateArray = [];
-let coordinates = { };
 
+var megaObj = {};
 
-app.get('/readTheLines', (request, response) => {
+app.get('/getit', (request, response) => {
+
 
   lineReader.eachLine('./FacebookPlaces_Albuquerque.csv', function(line, last) {
 
-        for (let i = 0; i < line.length; i++) {
-          // console.log("in tha loop.......");
-          if ((line[i] === ',' && line[i+1] === '-') || (line[i] === ',' && !isNaN(line[i+1]))) {
-            console.log("its a comma followed by a number or - sign");
-            // console.log(isNaN(line[i+2]));
-            if ((!isNaN(line[i+2]) && !isNaN(line[i+3])) || (!isNaN(line[i+2]) && line[i+3] === '.' )) {
-              console.log("definitely a number!!!");
-              let coords = line.substring(i+1)
-              let bizName = line.substring(0, i);
-
-              console.log("coordinates ", coords);
-              console.log("biz name? ", bizName);
-              // coordinateArray.push(coords);
-            }
-
-
-            // coordinateArray.push()
+      let lineAry = [];
+      ////////// filter original lines, push them to new array
+      for (let i = line.length -1; i >= line.length -3; i--) {
+        if (line[line.length-1] === ',' && line[line.length-2] === ',') {
+          let formattedLine = line.substring(0, line.length - 3);
+          lineAry.push(formattedLine);
+        } else if (line[line.length-1] === ',' && !isNaN(line[line.length-2])) {
+          let formattedLine = line.substring(0, line.length - 1);
+          lineAry.push(formattedLine)
+        } else {
+          let formattedLine = line.substring(0, line.length);
+          lineAry.push(formattedLine);
         }
       }
-      // console.log(coordinateArray[0], coordinateArray[1]);
-    // }
-    if(last){
-      // or check if it's the last one
-    }
-  })
+      ///////// take array of filtered lines, loop through them, find the
+      ////////  index we care about (where the 2nd comma is), store that index
+      ////////  use that index to split the string into parts you care about
+      ////////  put those string parts into the megaObj
+      let indy;
+      let commaCount = 0;
+      for (let i = lineAry.length-1; i > 0; i--) {
+        for (let j = lineAry[i].length -1; j >= 0; j--) {
+          if (commaCount < 2) {
+            if (lineAry[i][j] === ',') {
+              commaCount++;
+              indy = j;
+            }
+          }
+        }
+        let bizName = lineAry[i].substring(0, indy);
+        let bizCoords = lineAry[i].substring(indy+1, lineAry[i].length);
+        megaObj[bizName] = bizCoords;
+      }
+      //////////// log all the data
+
+  /////// write new file based on the filtered data
+})
+fs.writeFile('formattedData.json', JSON.stringify(megaObj, null, 4), function(err) {
+  console.log('File successfully written! - Check your project directory for the formattedData.json file!');
+})
 })
 
-app.get('/poop', (request, response) => {
-  response.json({
-    message: 'Youve made it to the hidden endpoint!'
-  })
-})
 
-function notFound(request, response, next) {
-  response.status(404);
-  const error = new Error('Not Found - ' + request.originalUrl);
-  next(error);
-}
-
-function errorHandler(error, request, response, next) {
-  response.status(response.statusCode || 500);
-  response.json({
-    message: error.message,
-    stack: error.stack
-  });
-}
-
-app.use(notFound);
-app.use(errorHandler);
+// BOILERPLATE EXPRESS SERVER STUFF
+// app.get('/poop', (request, response) => {
+//   response.json({
+//     message: 'Youve made it to the hidden endpoint!'
+//   })
+// })
+//
+// function notFound(request, response, next) {
+//   response.status(404);
+//   const error = new Error('Not Found - ' + request.originalUrl);
+//   next(error);
+// }
+//
+// function errorHandler(error, request, response, next) {
+//   response.status(response.statusCode || 500);
+//   response.json({
+//     message: error.message,
+//     stack: error.stack
+//   });
+// }
+//
+// app.use(notFound);
+// app.use(errorHandler);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
